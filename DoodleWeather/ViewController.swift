146296d2886034d35bfa68c566f30db774e21a1d
Сергей Морозов <sergey@MacBook-Pro-Sergej.local>
2017-12-10 21:45:11 +0300

    Initial Commit

---
+++ DoodleWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoodleWeather
+//
+//  Created by Сергей Морозов on 10.12.17.
+//  Copyright © 2017 Сергей Морозов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
