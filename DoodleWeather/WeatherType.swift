6d692e37428ec808e9422cd60244456160aec316
Сергей Морозов <sergey@MacBook-Pro-Sergej.local>
2017-12-23 14:27:57 +0300

    added pictures

--- DoodleWeather/WeatherType.swift
+++ DoodleWeather/WeatherType.swift
@@ -17,30 +17,63 @@ enum WeatherType {
     static let IMAGE_NAME_UNKNOWN = "unknown"
     
     static let CODE_TO_TYPE: [Int:WeatherType] = [
-        26:cloudy,
-        27:cloudy,
-        28:cloudy,
-        29:cloudy,
-        30:cloudy,
+//        0    tornado
+//        1    tropical storm
+//        2    hurricane
+//        3    severe thunderstorms
+//        4    thunderstorms
+        5:rain,    //mixed rain and snow
+        6:rain,    //mixed rain and sleet
+        7:snow,    //mixed snow and sleet
+//        8    freezing drizzle
+//        9    drizzle
+        10:rain,    //freezing rain
+        11:rain,    //showers
+        12:rain,    //showers
+        13:snow,    //snow flurries
+        14:snow,    //light snow showers
+        15:snow,    //blowing snow
+        16:snow,    //snow
+//        17    hail
+//        18    sleet
+//        19    dust
+//        20    foggy
+//        21    haze
+//        22    smoky
+//        23    blustery
+//        24    windy
+//        25    cold
+        26:cloudy,    //cloudy
+        27:cloudy,    //mostly cloudy (night)
+        28:cloudy,    //mostly cloudy (day)
+        29:cloudy,    //partly cloudy (night)
+        30:cloudy,    //partly cloudy (day)
+//        31    clear (night)
+//        32    sunny
+//        33    fair (night)
+//        34    fair (day)
+//        35    mixed rain and hail
+//        36    hot
+//        37    isolated thunderstorms
+//        38    scattered thunderstorms
+//        39    scattered thunderstorms
+        40:rain,    //scattered showers
+        41:snow,    //heavy snow
+        42:snow,    //scattered snow showers
+        43:snow,    //heavy snow
+//        44    partly cloudy
+//        45    thundershowers
+        46:snow,    //snow showers
+//        47    isolated thundershowers
+//        3200    not available
         
-        7:snow,
-        13:snow,
-        14:snow,
-        15:snow,
-        16:snow,
-        41:snow,
-        42:snow,
-        43:snow,
-        46:snow,
-        
-        5:rain,
-        6:rain,
-        10:rain,
-        35:rain,
-        11:rain,
-        12:rain,
-        40:rain
-        
+    ]
+    
+    static let TYPE_TO_IMAGE_NAME: [WeatherType:String] = [
+        .cloudy: "cloudy",
+        .rain: "rain",
+        .snow: "snow",
+        .unknown: IMAGE_NAME_UNKNOWN
     ]
     
     static func fromCode(_ code: Int) -> WeatherType {
@@ -48,71 +81,7 @@ enum WeatherType {
     }
     
     func getImageName() -> String {
-        switch self {
-        case .cloudy:
-            return "cloud"
-        default:
-            return WeatherType.IMAGE_NAME_UNKNOWN
-        }
+        return WeatherType.TYPE_TO_IMAGE_NAME[self] ?? WeatherType.IMAGE_NAME_UNKNOWN
     }
-    
-    //    (26, "cloudy")
-    
-    //    init(code: Int, description: String) {
-    //
-    //    }
-    
-    /*
-     
-     Code    Description
-     0    tornado
-     1    tropical storm
-     2    hurricane
-     3    severe thunderstorms
-     4    thunderstorms
-     5    mixed rain and snow
-     6    mixed rain and sleet
-     7    mixed snow and sleet
-     8    freezing drizzle
-     9    drizzle
-     10    freezing rain
-     11    showers
-     12    showers
-     13    snow flurries
-     14    light snow showers
-     15    blowing snow
-     16    snow
-     17    hail
-     18    sleet
-     19    dust
-     20    foggy
-     21    haze
-     22    smoky
-     23    blustery
-     24    windy
-     25    cold
-     26    cloudy
-     27    mostly cloudy (night)
-     28    mostly cloudy (day)
-     29    partly cloudy (night)
-     30    partly cloudy (day)
-     31    clear (night)
-     32    sunny
-     33    fair (night)
-     34    fair (day)
-     35    mixed rain and hail
-     36    hot
-     37    isolated thunderstorms
-     38    scattered thunderstorms
-     39    scattered thunderstorms
-     40    scattered showers
-     41    heavy snow
-     42    scattered snow showers
-     43    heavy snow
-     44    partly cloudy
-     45    thundershowers
-     46    snow showers
-     47    isolated thundershowers
-     3200    not available
-     */
+   
 }
